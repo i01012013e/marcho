680e4c1b6a6416a562daaf81c8d29ad138bd8433
Your Name <you@example.com>
2022-03-19 19:28:47 +0300

    fix btn and added section product

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,19 @@
 $(function () {
     $(".top-slider__inner").slick({
-      dots: true,
-      arrows: false,
-      // autoplay: true,
-      // autoplaySped: 1000,
+        dots: true,
+        arrows: false,
+        // autoplay: true,
+        // autoplaySped: 1000,
+    });
+
+    $(".price-box__card-star").rateYo({
+        starWidth: "17px",
+        normalFill: "#ccccce",
+        readOnly: true,
+
+        multiColor: {
+          "startColor": "#fe3e57" , //RED
+          "endColor"  : "#ffcc66" ,//YELLOW
+        }
     });
 });
